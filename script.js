6676de0c35ee9e00ec2f13e874aaf7ca94d1aa6c
Alexandra Aryshtaeva <alexandra.maranga@gmail.com>
2024-01-25 11:50:25 +0000

    basic html elements to start aplying the script code

---
+++ script.js
@@ -0,0 +1,13 @@
+// let board = [];
+
+// const note = writteTask("buy eggs", "buy milk");
+
+// function writteTask(task) {
+//   return board.push(task);
+// }
+
+// console.log(board);
+
+//
+
+document.querySelector();
