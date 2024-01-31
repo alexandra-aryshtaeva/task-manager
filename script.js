bd1f93cdb5eec48202d156bb235d940b64c94cc0
Alexandra Aryshtaeva <alexandra.maranga@gmail.com>
2024-01-31 14:27:16 +0000

    can add tasks but cant delete yet

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
+let input = document.querySelector("input");
+const button = document.querySelector("button");
+
+let list = document.getElementById("myList");
+
+button.addEventListener("click", () => {
+  const task = document.createElement("li");
+
+  const deleteBtn = document.createElement("button");
+
+  task.innerText = input.value;
+  deleteBtn.innerText = "delete";
+
+  input.value = "";
+  if (task.innerText === "") {
+    return;
+  }
+
+  list.appendChild(task);
+  task.appendChild(deleteBtn);
+
+  deleteBtn.addEventListener("click", () => {
+    let newList = Array.from(list.children);
+    //
+    newList = newList.pop();
+    return (list.innerHTML = newList.innerHTML);
+  });
+});
+
+// ______________________________________________
 // let board = [];
+// let boardTasks = writteTask("water", "butter");
 
-// const note = writteTask("buy eggs", "buy milk");
-
-// function writteTask(task) {
-//   return board.push(task);
+// function writteTask(...task) {
+//   board.push(task);
+//   return (board = board[0]);
 // }
+// board.pop();
 
 // console.log(board);
-
-//
-
-document.querySelector();
